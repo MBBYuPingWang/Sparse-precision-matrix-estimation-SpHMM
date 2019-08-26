94d7982eecd61f2a3e03dd264f9ef6606bb1596b
gmeng92 <36212932+gmeng92@users.noreply.github.com>
2019-08-26 10:45:09 -0500

    Update decode_viterbi.m

--- maincodes/decode_viterbi.m
+++ maincodes/decode_viterbi.m
@@ -5,7 +5,7 @@ function subj_path = decode_viterbi(data, mu, Sigma, prior, transmat)
 %num_state = length(prior);
 num_time = size(data,2);
 num_subj = size(data,3);
-%path_str= ['roi85_path_full' num2str(num_cluster) '.mat'];
+
 subj_path= zeros(num_subj, num_time);
     for i=1: num_subj
         obslik = mixgauss_prob(data(:,:,i), mu, Sigma);
